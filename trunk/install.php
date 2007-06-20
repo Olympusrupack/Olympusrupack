6f92cc11871f22bf60942ee9d5be3d886716c9a9
coagulant <coagulant@e57a84d7-7524-0410-997a-f7906806d589>
2007-06-20 19:20:46 +0000

    Важные добавления. Пока не перевёл до конца.
    git-svn-id: https://olympusrupack.svn.sourceforge.net/svnroot/olympusrupack@232 e57a84d7-7524-0410-997a-f7906806d589

--- trunk/install.php
+++ trunk/install.php
@@ -268,7 +268,9 @@ $lang = array_merge($lang, array(
 	'NO_LOCATION'				=> 'Не удалось найти приложение. Если вы знаете, что Imagemagick установлен, вы можете указать путь к нему в администраторском разделе после установки.',
 	'NO_TABLES_FOUND'			=> 'Таблиц не найдено.',
 // TODO: Write some explanatory introduction text
+
 	'OVERVIEW_BODY'					=> 'Мы рады представить вам общедоступный релиз-кандидат phpBB следующего поколения после 2.0.x, phpBB 3.0! Этот релиз предназначен для широкой аудитории, он призван помочь нам обнаружить последние ошибки и проблемные участки кода.</p><p>Пожалуйста, прочтите <a href="../docs/INSTALL.html">руководство по установке</a> для получения дополнительной информации по установке phpBB3</p><p><strong style="text-transform: uppercase;">Примечание:</strong> Этот релиз <strong style="text-transform: uppercase;">по-прежнему не финальный</strong>. Возможно, вам стоит дождаться выхода окончательной полной версии, прежде чем вы начнёте пользоваться конференцией в реальных условиях.</p><p>Система установки будет помогать вам на протяжении процесса установки phpBB, конвертирования из другого программного обеспечения или обновления до последней версии phpBB. За дополнительной информацией по каждому из пунктов перейдите по соответствующей ссылке меню выше.',
+
 	'PCRE_UTF_SUPPORT'				=> 'Поддержка UTF-8 в PCRE',
 	'PCRE_UTF_SUPPORT_EXPLAIN'		=> 'phpBB <strong>не будет</strong> работать, если PHP собран без поддержки UTF-8 для расширения PCRE.',
 	'PHP_GETIMAGESIZE_SUPPORT'			=> 'Функция PHP getimagesize() доступна',
@@ -303,9 +305,9 @@ $lang = array_merge($lang, array(
 	'SCRIPT_PATH_EXPLAIN'		=> 'Путь к папке, содержащей phpBB, относительно корня сайта, например, <samp>/phpBB3</samp>.',
 	'SELECT_LANG'				=> 'Выберите язык',
 	'SERVER_CONFIG'				=> 'Конфигурация сервера',
-	'SOFTWARE'					=> 'Тип конференции',
 	'SEARCH_INDEX_UNCONVERTED'	=> 'Поисковый индекс не был сконвертирован',
 	'SEARCH_INDEX_UNCONVERTED_EXPLAIN'	=> 'Ваш старый поисковый индекс не был сконвертирован. Любой поисковый запрос вернёт пустой результат. Чтобы создать новый поисковый индекс, перейдите в администраторский раздел, выберите подраздел Обслуживание и в нём пункт Поисковый индекс.',
+	'SOFTWARE'					=> 'Тип конференции',
 	'SPECIFY_OPTIONS'			=> 'Определение настроек конвертирования',
 	'STAGE_ADMINISTRATOR'		=> 'Сведения об администраторе',
 	'STAGE_ADVANCED'			=> 'Расширенные настройки',
@@ -339,14 +341,13 @@ $lang = array_merge($lang, array(
 
 	'UNABLE_WRITE_LOCK'			=> 'Не удалось записать файл блокировки',
 	'UNAVAILABLE'				=> 'Недоступно',
-	'UNWRITEABLE'				=> 'Нет прав на запись',
+	'UNWRITABLE'				=> 'Нет прав на запись',
 	'UPDATE_TOPICS_POSTED'		=> 'Генерация перечня тем, в которых писал пользователь',
 	'UPDATE_TOPICS_POSTED_ERR'	=> 'Произошла ошибка при генерации перечня тем, в которых писал пользователь. Вы можете повторить попытку выполнить этот этап в администраторском разделе после завершения процесса конвертирования.',
-
 	'VERSION'					=> 'Версия',
 
 	'WELCOME_INSTALL'			=> 'Вас приветствует программа установки phpBB3',
-	'WRITEABLE'					=> 'Права на запись',
+	'WRITABLE'					=> 'Права на запись',
 ));
 
 // Updater
@@ -523,6 +524,58 @@ $lang = array_merge($lang, array(
 	'VIEWING_FILE_DIFF'			=> 'Просмотр различий в файлах',
 
 	'WRONG_INFO_FILE_FORMAT'	=> 'Ошибочный формат файла информации об обновлении',
+));
+
+// Default database schema entries...
+$lang = array_merge($lang, array(
+	'CONFIG_BOARD_EMAIL_SIG'		=> 'Cпасибо, Администрация',
+	'CONFIG_SITE_DESC'				=> 'A _little_ text to describe your forum',
+	'CONFIG_SITENAME'				=> 'yourdomain.com',
+
+	'DEFAULT_INSTALL_POST'			=> 'This is an example post in your phpBB3 installation. You may delete this post, this topic and even this forum if you like since everything seems to be working!',
+
+	'EXT_GROUP_ARCHIVES'			=> 'Архивы',
+	'EXT_GROUP_DOCUMENTS'			=> 'Документы',
+	'EXT_GROUP_DOWNLOADABLE_FILES'	=> 'Downloadable Files',
+	'EXT_GROUP_FLASH_FILES'			=> 'Flash Files',
+	'EXT_GROUP_IMAGES'				=> 'Изображения',
+	'EXT_GROUP_PLAIN_TEXT'			=> 'Plain Text',
+	'EXT_GROUP_QUICKTIME_MEDIA'		=> 'Quicktime Media',
+	'EXT_GROUP_REAL_MEDIA'			=> 'Real Media',
+	'EXT_GROUP_WINDOWS_MEDIA'		=> 'Windows Media',
+
+	'FORUMS_FIRST_CATEGORY'			=> 'My first Category',
+	'FORUMS_TEST_FORUM_DESC'		=> 'This is just a test forum.',
+	'FORUMS_TEST_FORUM_TITLE'		=> 'Test Forum 1',
+
+	'RANKS_SITE_ADMIN_TITLE'		=> 'Администратор',
+
+	'SMILIES_ARROW'					=> 'Arrow',
+	'SMILIES_CONFUSED'				=> 'Confused',
+	'SMILIES_COOL'					=> 'Cool',
+	'SMILIES_CRYING'				=> 'Crying or Very Sad',
+	'SMILIES_EMARRASSED'			=> 'Embarrassed',
+	'SMILIES_EVIL'					=> 'Evil or Very Mad',
+	'SMILIES_EXCLAMATION'			=> 'Exclamation',
+	'SMILIES_GEEK'					=> 'Geek',
+	'SMILIES_IDEA'					=> 'Idea',
+	'SMILIES_LAUGHING'				=> 'Laughing',
+	'SMILIES_MAD'					=> 'Mad',
+	'SMILIES_MR_GREEN'				=> 'Mr. Green',
+	'SMILIES_NEUTRAL'				=> 'Neutral',
+	'SMILIES_QUESTION'				=> 'Question',
+	'SMILIES_RAZZ'					=> 'Razz',
+	'SMILIES_ROLLING_EYES'			=> 'Rolling Eyes',
+	'SMILIES_SAD'					=> 'Sad',
+	'SMILIES_SHOCKED'				=> 'Shocked',
+	'SMILIES_SMILE'					=> 'Smile',
+	'SMILIES_SURPRISED'				=> 'Surprised',
+	'SMILIES_TWISTED_EVIL'			=> 'Twisted Evil',
+	'SMILIES_UBER_GEEK'				=> 'Uber Geek',
+	'SMILIES_VERY_HAPPY'			=> 'Very Happy',
+	'SMILIES_WINK'					=> 'Wink',
+
+	'TOPICS_TOPIC_TITLE'			=> 'Welcome to phpBB3',
 ));
 
 ?>
